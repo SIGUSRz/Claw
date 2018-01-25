504bfd270b46e4460f6dd979ab7f8e50b44b4439
sz1358 <sz1358@nyu.edu>
2018-01-25 15:58:50 +0800

    2.0.1: test push

--- clip_writer.py
+++ clip_writer.py
@@ -1,99 +1,83 @@
 # import the necessary packages
+from __future__ import print_function
+from imutils.video import VideoStream
+import numpy as np
 import argparse
-import datetime
 import imutils
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--output", required=True,
-                    help="path to output video file")
-parser.add_argument("-p", "--picamera", type=int, default=-1,
-                    help="whether or not the Raspberry Pi camera should be used")
-parser.add_argument("-f", "--fps", type=int, default=20,
-                    help="FPS of output video")
-parser.add_argument("-c", "--codec", type=str, default="MJPG",
-                    help="codec of output video")
-parser.add_argument("-v", "--video", default=None,
-                    help="path to the video file")
-parser.add_argument("-a", "--min-area", type=int, default=500,
-                    help="minimum area size")
-args = vars(parser.parse_args())
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", required=True,
+                help="path to output video file")
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+                help="whether or not the Raspberry Pi camera should be used")
+ap.add_argument("-f", "--fps", type=int, default=20,
+                help="FPS of output video")
+ap.add_argument("-c", "--codec", type=str, default="MJPG",
+                help="codec of output video")
+args = vars(ap.parse_args())
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    camera = cv2.VideoCapture(0)
-    time.sleep(0.25)
+# initialize the video stream and allow the camera
+# sensor to warmup
+print("[INFO] warming up camera...")
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+time.sleep(2.0)
 
-# otherwise, we are reading from a video file
-else:
-    camera = cv2.VideoCapture(args["video"])
+# initialize the FourCC, video writer, dimensions of the frame, and
+# zeros array
+fourcc = cv2.VideoWriter_fourcc(*args["codec"])
+(h, w) = (400, 500)
+writer = cv2.VideoWriter(args["output"], fourcc, args["fps"],
+                         (w * 2, h * 2), True)
+zeros = None
 
-# initialize the first frame in the video stream
-firstFrame = None
-
-# loop over the frames of the video
+# loop over frames from the video stream
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    (grabbed, frame) = camera.read()
-    text = "Unoccupied"
+    # grab the frame from the video stream and resize it to have a
+    # maximum width of 300 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, height=h, width=w)
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if not grabbed:
-        break
+    # check if the writer is None
+    # store the image dimensions, initialzie the video writer,
+    # and construct the zeros array
+    (h, w) = frame.shape[:2]
 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    zeros = np.zeros((h, w), dtype="uint8")
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
+    # break the image into its RGB components, then construct the
+    # RGB representation of each frame individually
+    (B, G, R) = cv2.split(frame)
+    R = cv2.merge([zeros, zeros, R])
+    G = cv2.merge([zeros, G, zeros])
+    B = cv2.merge([B, zeros, zeros])
 
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    # construct the final output frame, storing the original frame
+    # at the top-left, the red channel in the top-right, the green
+    # channel in the bottom-right, and the blue channel in the
+    # bottom-left
+    output = np.zeros((h * 2, w * 2, 3), dtype="uint8")
+    output[0:h, 0:w] = frame
+    output[0:h, w:w * 2] = R
+    output[h:h * 2, w:w * 2] = G
+    output[h:h * 2, 0:w] = B
 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    (img, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
+    # write the output frame to file
+    writer.write(output)
 
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
+    # show the frames
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Output", output)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key is pressed, break from the lop
+    # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
 
-# cleanup the camera and close any open windows
-camera.release()
+# do a bit of cleanup
+print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
+vs.stop()
+writer.release()
