4e7102d3064959fe1fb1d5e28a92f25b19b07ef5
sz1358 <sz1358@nyu.edu>
2018-01-26 13:58:07 +0800

    2.0.18: test push

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #python video_writer.py --output ~/Videos/test.avi --picamera 1
 #python motion_detect.py --video ~/Videos/test.avi
 #python clip_writer.py --output ~/Videos/test --picamera 1
-python clip_writer.py --output ~/Videos/test --picamera 1
+python fast_writer.py --output ~/Videos/test --picamera 1
