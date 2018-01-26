bb812445f9a891dcb13a641ea7876ed5e5b25784
sz1358 <sz1358@nyu.edu>
2018-01-26 14:06:23 +0800

    2.0.21: test push

--- fast_writer.py
+++ fast_writer.py
@@ -9,86 +9,102 @@ import imutils
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-                help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-                help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
 
-# initialize the camera and stream
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(320, 240))
-stream = camera.capture_continuous(rawCapture, format="bgr",
-                                   use_video_port=True)
+def main():
+    # initialize the camera and stream
+    camera = PiCamera()
+    camera.resolution = (320, 240)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(320, 240))
+    stream = camera.capture_continuous(rawCapture, format="bgr",
+                                       use_video_port=True)
 
-# allow the camera to warmup and start the FPS counter
-print("[INFO] sampling frames from `picamera` module...")
-time.sleep(2.0)
-fps = FPS().start()
+    # allow the camera to warmup and start the FPS counter
+    print("[INFO] sampling frames from `picamera` module...")
+    time.sleep(2.0)
+    fps = FPS().start()
 
-# loop over some frames
-for (i, f) in enumerate(stream):
-    # grab the frame from the stream and resize it to have a maximum
-    # width of 400 pixels
-    frame = f.array
-    frame = imutils.resize(frame, width=400)
+    # loop over some frames
+    for (i, f) in enumerate(stream):
+        # grab the frame from the stream and resize it to have a maximum
+        # width of 400 pixels
+        frame = f.array
+        frame = imutils.resize(frame, width=400)
 
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+        # check to see if the frame should be displayed to our screen
+        if args["display"] > 0:
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
 
-    # clear the stream in preparation for the next frame and update
-    # the FPS counter
-    rawCapture.truncate(0)
-    fps.update()
+        # clear the stream in preparation for the next frame and update
+        # the FPS counter
+        rawCapture.truncate(0)
+        fps.update()
 
-    # check to see if the desired number of frames have been reached
-    if i == args["num_frames"]:
-        break
+        # check to see if the desired number of frames have been reached
+        if i == args["num_frames"]:
+            break
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-stream.close()
-rawCapture.close()
-camera.close()
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    stream.close()
+    rawCapture.close()
+    camera.close()
 
-# created a *threaded *video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from `picamera` module...")
-vs = PiVideoStream().start()
-time.sleep(2.0)
-fps = FPS().start()
+    # created a *threaded *video stream, allow the camera sensor to warmup,
+    # and start the FPS counter
+    print("[INFO] sampling THREADED frames from `picamera` module...")
+    vs = PiVideoStream().start()
+    time.sleep(2.0)
+    fps = FPS().start()
 
-# loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    # loop over some frames...this time using the threaded stream
+    while fps._numFrames < args["num_frames"]:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
 
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+        # check to see if the frame should be displayed to our screen
+        if args["display"] > 0:
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
 
-    # update the FPS counter
-    fps.update()
+        # update the FPS counter
+        fps.update()
 
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+
+
+if __name__ == "__main__":
+    # construct the argument parse and parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", required=True,
+                        help="path to output video file")
+    parser.add_argument("-p", "--picamera", type=int, default=-1,
+                        help="whether or not the Raspberry Pi camera should be used")
+    parser.add_argument("-f", "--fps", type=int, default=30,
+                        help="FPS of output video")
+    parser.add_argument("-c", "--codec", type=str, default="MJPG",
+                        help="codec of output video")
+
+    parser.add_argument("-n", "--num-frames", type=int, default=100,
+                        help="# of frames to loop over for FPS test")
+    parser.add_argument("-d", "--display", type=int, default=-1,
+                        help="Whether or not frames should be displayed")
+
+    args = vars(parser.parse_args())
+
+    main(args)
