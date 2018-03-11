961522687377258b5bdd5b54bdade68e7fdf4bae
sz1358 <sz1358@nyu.edu>
2018-03-11 20:04:06 +0800

    2.3.4: add video file clean

--- build_rasp.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D PYTHON_DEFAULT_EXECUTABLE=/home/pi/.virtualenvs/py3/bin/python3 -D WITH_FFMPEG=ON -D BUILD_EXAMPLES=ON ..
