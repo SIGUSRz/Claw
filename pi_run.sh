fa798a935ac4d74542b96ee93b03ddd3fb604ffd
sz1358 <sz1358@nyu.edu>
2018-03-30 13:13:14 +0800

    3.1: change script setting

--- pi_run.sh
+++ pi_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #!/usr/bin/env bash
-python pi_writer.py --output ~/Videos/train --picamera True
+python pi_writer.py --output ~/Videos/train --picamera True --display True
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python pi_writer.py --output ~/Videos/train
+python pi_writer.py --output ~/Videos/train --display True
